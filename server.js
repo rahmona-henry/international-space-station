7d1aeb6e165677c1acf4cd62b1cb8de328472bf3
rahmona-henry <rahmona.henry@icloud.com>
2016-08-08 18:45:26 +1200

    Add xhr module

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
+var xhr = require('xhr')
 
 app.use(express.static('client'))
 
 app.get('/', function(req, res){
   res.send('index.html')
+})
+
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err,data){
+  if(err) {
+    console.log('error', err)
+  }
+  console.log('this is data', data)
+
 })
 
 app.listen(3000, function(){
